3588b74f7cea94036017e858df6457557df8f76e
ansaws <junk.ansaws@gmail.com>
2021-10-02 17:25:58 -0500

    Obj-gen done
    distance away from each other

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,45 @@ public class CameraController : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject player;
+    public GameObject[] objs;
+    public GameObject objPrefab;
     // Start is called before the first frame update
     void Start()
     {
         player = Instantiate(playerPrefab);
+        objs = GameObject.FindGameObjectsWithTag("Object");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        objs = GameObject.FindGameObjectsWithTag("Object");
+        if (Input.GetKeyDown("space"))
+        {
+            objSpawn();
+        }
     }
+
+    public void objSpawn()
+    {
+
+        float randx = Random.Range(-.5f, .5f);
+        float randy = Random.Range(-.5f, .5f);
+        Vector3  mousePos = GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+        Vector3 spawnpos = new Vector3(mousePos.x + randx, mousePos.y + randy, 0);
+        foreach (GameObject g in objs)
+        {
+            while(Vector3.Distance(g.transform.position, spawnpos) < 1)
+            {
+                spawnpos.x += .2f;
+                spawnpos.y += -.3f;
+            }
+        }
+       
+        GameObject temp = Instantiate(objPrefab);
+        Debug.Log(spawnpos);
+        temp.transform.position = spawnpos;
+    }
+    
 }
