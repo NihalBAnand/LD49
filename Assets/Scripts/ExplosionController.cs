5f97830447a18f0055bdb9063ee296e0f2033bf8
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2021-10-02 15:23:39 -0500

    Anish can't make programmer art lol

--- Assets/Scripts/ExplosionController.cs
+++ Assets/Scripts/ExplosionController.cs
@@ -7,20 +7,20 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(OnCompleteAttackAnimation());
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.GetComponent<Animator>().Play("Explosion");
-        StartCoroutine(OnCompleteAttackAnimation());
     }
     IEnumerator OnCompleteAttackAnimation()
     {
         while (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
-            
+        {
+            gameObject.GetComponent<Animator>().Play("Explosion");
             yield return null;
+        }
 
         Destroy(gameObject);
     }
