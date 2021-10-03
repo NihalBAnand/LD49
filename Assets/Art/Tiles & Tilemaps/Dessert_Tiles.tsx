17ded8a404d8aef5f373ab69a35b201e1160a57f
ansaws <junk.ansaws@gmail.com>
2021-10-03 11:53:36 -0500

    sand tileset created + 1/2 of desert biome

---
+++ Assets/Art/Tiles & Tilemaps/Dessert_Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Dessert_Tiles" tilewidth="32" tileheight="32" tilecount="378" columns="18">
+ <image source="SAND tiles.png" width="576" height="672"/>
+</tileset>
