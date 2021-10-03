8829c94030a80fd72fff4c820fd3eda04f6f290f
ansaws <junk.ansaws@gmail.com>
2021-10-03 16:10:40 -0500

    stuff

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -10,7 +10,6 @@ public class ObjectSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         objs = GameObject.FindGameObjectsWithTag("Object");
     }
 
@@ -23,7 +22,7 @@ public class ObjectSpawner : MonoBehaviour
 
     public void objSpawn()
     {
-        /*
+        
         float randx = Random.Range(-.5f, .5f);
         float randy = Random.Range(-.5f, .5f);
         Vector3 spawnpos = new Vector3(player.transform.x + randx, player.transform.y + randy, 0);
@@ -38,6 +37,14 @@ public class ObjectSpawner : MonoBehaviour
 
         GameObject temp = Instantiate(objPrefab);
         temp.transform.position = spawnpos;
-        */
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Console.Log("agdfskljagljkfd");
+            objSpawn();
+        }
     }
 }
