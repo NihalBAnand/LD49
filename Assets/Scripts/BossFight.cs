3d506bace8f2e1f5e20d57d9c8f07e47963cf615
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2021-10-04 13:02:51 -0500

    Boss fight

---
+++ Assets/Scripts/BossFight.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFight : MonoBehaviour
+{
+    public GameObject bossPrefab;
+
+    public AudioClip intro;
+    public AudioClip loop;
+    public AudioClip finale;
+
+    public AudioSource source;
+    public bool started;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        started = false;
+        player = GameObject.Find("Player(Clone)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("Player(Clone)");
+        if (started)
+        {
+            if (GameObject.Find("Boss(Clone)") == null)
+            {
+                source.loop = false;
+                source.Stop();
+                source.PlayOneShot(finale);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !started)
+        {
+            player.transform.GetChild(1);
+            started = true;
+            GameObject boss = Instantiate(bossPrefab);
+            boss.transform.position = transform.position;
+            source.PlayOneShot(intro);
+
+            StartCoroutine(PlayLoop());
+        }
+    }
+
+    private IEnumerator PlayLoop()
+    {
+        while (source.isPlaying)
+        {
+            yield return null;
+        }
+        source.PlayOneShot(loop);
+        source.loop = true;
+        
+    }
+}
